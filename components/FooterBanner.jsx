69c29691181c6c24d96286b106399b0842b2b80d
Rafael Estrada <rafaelestrada@comcast.net>
2022-07-03 22:11:34 -0700

    addes footer banner and img

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { urlFor } from '../lib/client';
 
-const FooterBanner = () => {
+const FooterBanner = ({footerBanner}) => {
   return (
-    <div>FooterBanner</div>
+    <div className='footer-banner-container'>
+
+      <div className='banner-desc'>
+        <div className='left'>
+          <p>{footerBanner.discount}</p>
+          <h3>{footerBanner.largeText1}</h3>
+
+        </div>
+        <div className='right'>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import { urlFor } from '../lib/client';
 
 const HeroBanner = ({heroBanner}) => {
   return (
@@ -8,19 +9,20 @@ const HeroBanner = ({heroBanner}) => {
       <div>
       <p className='beats-solo'>{heroBanner.smallText}</p>
 
-      <h3>MID TEXT</h3>
-      <img src='' alt='headphones'
+      <h3>{heroBanner.midText}</h3>
+      <h1>{heroBanner.largeText1}</h1>
+      <img src={urlFor(heroBanner.image)} alt='headphones'
       className='hero-banner-image'/>
       <div>
-        <Link href='/product/ID'>
-          <button type='button'>BUTTON TEXT
+        <Link href={`/product/${heroBanner.product}`}>
+          <button type='button'>{heroBanner.buttonText}
 
           </button>
         
         </Link>
         <div className='desc'>
-          <h5>Description</h5>
           <p>DESCRIPTION</p>
+          <h5>{heroBanner.desc}</h5>
 
         </div>
       </div>

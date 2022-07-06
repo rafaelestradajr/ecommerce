dfd33f43b240eb2fe0da20e6adee62c8dd17d990
Rafael Estrada <rafaelestrada@comcast.net>
2022-07-05 21:55:18 -0700

    added product pages and details

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -2,19 +2,33 @@ import React from 'react';
 import Link from 'next/link';
 import { urlFor } from '../lib/client';
 
-const FooterBanner = ({footerBanner}) => {
+const FooterBanner = ({footerBanner:{discount,LargeText1,
+LargeText2,saleTime,smallText,midText,desc, product,buttonText,image}}) => {
   return (
     <div className='footer-banner-container'>
 
       <div className='banner-desc'>
         <div className='left'>
-          <p>{footerBanner.discount}</p>
-          <h3>{footerBanner.largeText1}</h3>
+          <p>{discount}</p>
+          <h3>{LargeText1}</h3>
+          <h3>{LargeText2}</h3>
+          <p>{saleTime}</p>
 
         </div>
         <div className='right'>
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type='button'>{buttonText}</button>
+
+          </Link>
 
         </div>
+        <img
+        src={urlFor(image)}
+        className='footer-banner-image'
+        />
 
       </div>
     </div>

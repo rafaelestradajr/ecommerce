c94fbbd8bcbfd29b9f721df4733f2d6c84de035b
Rafael Estrada <rafaelestrada@comcast.net>
2022-07-13 14:16:33 -0700

    added context

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -11,7 +11,7 @@ import { urlFor } from '../lib/client';
 const Cart = () => {
 
   const cartRef = useRef();
-  const {totalPrice,totalQuantities, cartItems, setShowCart} =
+  const {totalPrice,totalQuantities, cartItems, setShowCart,toggleCartItemQuantity,onRemove} =
   useStateContext();
   return (
     <div className= 'cart-wrapper' ref={cartRef}>
@@ -56,7 +56,7 @@ const Cart = () => {
                   
                   <p className='quantity-desc'>
             <span className="minus"
-            onClick=''>
+            onClick={() => toggleCartItemQuantity(item._id,'dec') }>
               <AiOutlineMinus/>
               
             </span>
@@ -66,7 +66,7 @@ const Cart = () => {
               
             </span>
             <span className="plus"
-            onClick=''>
+             onClick={() => toggleCartItemQuantity(item._id,'inc') }>
               <AiOutlinePlus/>              
             </span>
           </p>                  
@@ -75,8 +75,8 @@ const Cart = () => {
                   <button 
                   type='button'
                   className='remove-item'
-                  onClick=''
-                  >
+                  onClick={() => onRemove(item)}
+                  > 
                     <TiDeleteOutline/>
                   </button>
               </div>
